7f11cde2a64f467bf79b3cd317a1f6ece741fbb3
Henrique Dias <henrique.dias@rede.ulbra.br>
2023-07-19 16:58:44 -0300

    first commit

---
+++ src/app/api/admin/posts/[id]/unpublish/route.js
@@ -0,0 +1,24 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+export async function POST(request, { params }) {
+    const session = await getServerSession(authOptions);
+    const { id } = params;
+    const result = await prisma.post.update({
+        where: {
+            id: id,
+        },
+        data: {
+            published: false,
+        },
+        include: {
+            author: {
+                select: { name: true },
+            },
+        },
+    });
+    revalidatePath("/");
+    return NextResponse.json(result);
+}
---
+++ src/app/api/admin/posts/route.js
@@ -0,0 +1,19 @@
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+export async function POST(request) {
+    const session = await getServerSession(authOptions);
+    const { title, content } = await request.json();
+    const result = await prisma.post.create({
+        data: {
+            title: title,
+            content: content,
+            author: { connect: { email: session?.user?.email } },
+        },
+    });
+    revalidatePath("/");
+    return NextResponse.json(result);
+}
